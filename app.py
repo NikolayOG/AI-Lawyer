ee4ab79395f414c3ab2b1998976c08f760ac7064
Nikolay Gospodinov <nikolay.o.gospodinov@gmail.com>
2019-02-24 11:22:26 +0000

    pdf documents

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-from flask import Flask, request
+from flask import Flask, request, send_from_directory, send_file
 import spacy
 from dateparser import parse
 from nltk import word_tokenize, ngrams
@@ -7,10 +7,13 @@ import nltk
 import string
 from nltk.stem import WordNetLemmatizer
 from gensim.models import KeyedVectors
+import random
+import pdfrw
+from nameparser import HumanName
 wordnet_lemmatizer = WordNetLemmatizer()
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/documents')
 spacy_nlp = spacy.load('en')
 
 # topics
@@ -37,9 +40,9 @@ REASONS = {
     'children': CHILDREN
 }
 
-print('Loading model...')
-model = KeyedVectors.load_word2vec_format('./crawl-300d-2M.vec')
-print('Model loaded.')
+# print('Loading model...')
+# model = KeyedVectors.load_word2vec_format('./crawl-300d-2M.vec')
+# print('Model loaded.')
 
 
 def clean_text(text):
@@ -82,14 +85,12 @@ def extract_reasons():
         else:
             return jsonify({"status": str(-1)})
 
-
-
     except Exception as e:
         return e
 
 
-@app.route('/address', methods=['POST'])
-def parse_address():
+@app.route('/date', methods=['POST'])
+def parse_date():
     try:
         content = request.json
         text_date = content['text_date']
@@ -116,12 +117,69 @@ def parse_address():
                     date = str(element)
 
         if date:
-            return jsonify({"status": str(1), "date": str(parse(str(date)))})
+            return jsonify({"status": str(1), "date": str(parse(str(date))).split(' ')[0]})
         else:
             return jsonify({"status": str(-1)})
 
     except Exception as e:
         return e
+
+
+@app.route('/get_document', methods=['POST'])
+def get_document():
+    try:
+        content = request.json
+        claimant_name = content['claimant_name']
+
+        name = HumanName(claimant_name)
+
+        claimant_first_name = name.first
+        claimant_last_name = name.last
+
+        claimant_dispute_arguments = content['dispute_argument']
+
+        fill_in_values = {
+            'claimant2[first]': claimant_first_name,
+            'claimant2[last]': claimant_last_name,
+            'iDispute8': claimant_dispute_arguments,
+            'claimNo5': random.randint(1, 10000000000)
+        }
+
+
+        ANNOT_KEY = '/Annots'
+        ANNOT_FIELD_KEY = '/T'
+        ANNOT_VAL_KEY = '/V'
+        ANNOT_RECT_KEY = '/Rect'
+        SUBTYPE_KEY = '/Subtype'
+        WIDGET_SUBTYPE_KEY = '/Widget'
+
+        template_pdf = pdfrw.PdfReader('./defence-form.pdf')
+        annotations = template_pdf.pages[0][ANNOT_KEY]
+        for annotation in annotations:
+            if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
+                if annotation[ANNOT_FIELD_KEY]:
+                    key = annotation[ANNOT_FIELD_KEY][1:-1]
+                    if key in fill_in_values:
+                        annotation.update(
+                            pdfrw.PdfDict(V='{}'.format(fill_in_values[key]))
+                            )
+
+        filename = './documents/' + str(fill_in_values['claimNo5']) + '.pdf'
+
+        pdfrw.PdfWriter().write(filename,  template_pdf)
+
+        return '/document/' + str(fill_in_values['claimNo5']) + '.pdf'
+
+    except Exception as e:
+        return e
+
+
+@app.route('/document/<int:document_id>.pdf', methods=['GET'])
+def document(document_id):
+    print('ehrhehrehrhe')
+    document_id = str(document_id)
+    print()
+    return send_from_directory('./documents', document_id + '.pdf', )
 
 
 if __name__ == '__main__':
